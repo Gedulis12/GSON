d01ae92ebc54da3aafd0d53ee15fe1d8785a53e0
Gediminas Repecka <gediminas.repecka@gmail.com>
2024-01-29 17:46:19 +0200

    parser implementation work in progress

--- gson.c
+++ gson.c
@@ -27,7 +27,14 @@ typedef struct {
     int line;
 } Scanner;
 
-static Scanner* init_scanner(const char *source)
+typedef struct {
+    Token current;
+    Token previous;
+    int depth;
+    bool had_error;
+} Parser;
+
+static Scanner* scanner_init(const char *source)
 {
     Scanner *scanner = (Scanner*)malloc(sizeof(Scanner));
     scanner->start = source;
@@ -36,7 +43,7 @@ static Scanner* init_scanner(const char *source)
     return scanner;
 }
 
-static Token make_token(TokenType type, Scanner *scanner)
+static Token token_make(TokenType type, Scanner *scanner)
 {
     Token token;
     token.type = type;
@@ -47,7 +54,7 @@ static Token make_token(TokenType type, Scanner *scanner)
     return token;
 }
 
-static Token error_token(Scanner *scanner, const char *message)
+static Token token_error(Scanner *scanner, const char *message)
 {
     Token token;
     token.type = TOKEN_ERROR;
@@ -57,7 +64,7 @@ static Token error_token(Scanner *scanner, const char *message)
     return token;
 }
 
-static bool is_at_end(Scanner *scanner)
+static bool scanner_is_at_end(Scanner *scanner)
 {
     return *scanner->current == '\0';
 }
@@ -69,17 +76,17 @@ static char peek(Scanner *scanner)
 
 static char peek_next(Scanner *scanner)
 {
-    if (is_at_end(scanner)) return '\0';
+    if (scanner_is_at_end(scanner)) return '\0';
     return scanner->current[1];
 }
 
-static char advance(Scanner *scanner)
+static char scanner_advance(Scanner *scanner)
 {
     scanner->current++;
     return scanner->current[-1];
 }
 
-static void skip_white_space(Scanner *scanner)
+static void scanner_skip_white_space(Scanner *scanner)
 {
     for (;;)
     {
@@ -89,11 +96,11 @@ static void skip_white_space(Scanner *scanner)
             case ' ':
             case '\r':
             case '\t':
-                advance(scanner);
+                scanner_advance(scanner);
                 break;
             case '\n':
                 scanner->line++;
-                advance(scanner);
+                scanner_advance(scanner);
                 break;
             default:
                 return;
@@ -103,7 +110,7 @@ static void skip_white_space(Scanner *scanner)
 
 static bool match(char expected, Scanner *scanner)
 {
-    if (is_at_end(scanner)) return false;
+    if (scanner_is_at_end(scanner)) return false;
     if (*scanner->current != expected) return false;
     scanner->current++;
     return true;
@@ -114,101 +121,128 @@ static bool is_digit(char c)
     return c >= '0' && c <= '9';
 }
 
-static Token string(Scanner *scanner, char quoteSymbol)
+static bool is_hex(char c)
 {
-    while ((peek(scanner) != '"') && !is_at_end(scanner))
-    {
-        if (peek(scanner) == '\n') scanner->line++;
-        advance(scanner);
-    }
-
-    if (is_at_end(scanner)) return error_token(scanner, "Unterminated string");
-
-    if (advance(scanner) != quoteSymbol)
-    {
-        return error_token(scanner, "String quoted with non-matching quotes");
-    }
-    return make_token(TOKEN_STRING, scanner);
+    return (c >= 'a' && c <= 'f') ||
+           (c >= 'A' && c <= 'F') ||
+           (c >= '0' && c <= '9');
 }
 
-static Token number(Scanner *scanner)
+//TODO: handle the escape sequences correctly
+static Token token_string(Scanner *scanner)
 {
-    while (is_digit(peek(scanner))) advance(scanner);
+    while ((peek(scanner) != '"') && !scanner_is_at_end(scanner))
+    {
+        if (peek(scanner) == '\n') scanner->line++;
+        scanner_advance(scanner);
+    }
+
+    if (scanner_is_at_end(scanner)) return token_error(scanner, "Unterminated string");
+
+    scanner_advance(scanner);
+    return token_make(TOKEN_STRING, scanner);
+}
+
+static Token token_number(Scanner *scanner)
+{
+    while (is_digit(peek(scanner))) scanner_advance(scanner);
 
     if (peek(scanner) == '.' && is_digit(peek_next(scanner)))
     {
-        advance(scanner);
-        while (is_digit(peek(scanner))) advance(scanner);
+        scanner_advance(scanner);
+        while (is_digit(peek(scanner))) scanner_advance(scanner);
     }
 
-    return make_token(TOKEN_NUMBER, scanner);
+    return token_make(TOKEN_NUMBER, scanner);
 }
 
-static Token boolean(Scanner *scanner)
+static Token token_boolean(Scanner *scanner)
 {
     const char *true_str = "true";
     const char *false_str = "false";
 
     if (strncmp(true_str, scanner->current - 1, strlen(true_str)) == 0)
     {
-        while (peek(scanner) != 'e') advance(scanner);
-        advance(scanner);
-        return make_token(TOKEN_TRUE, scanner);
+        while (peek(scanner) != 'e') scanner_advance(scanner);
+        scanner_advance(scanner);
+        return token_make(TOKEN_TRUE, scanner);
     }
     else if (strncmp(false_str, scanner->current - 1, strlen(false_str)) == 0)
     {
-        while (peek(scanner) != 'e') advance(scanner);
-        advance(scanner);
-        return make_token(TOKEN_FALSE, scanner);
+        while (peek(scanner) != 'e') scanner_advance(scanner);
+        scanner_advance(scanner);
+        return token_make(TOKEN_FALSE, scanner);
     }
 
-    return error_token(scanner, "Unexpected keyword");
+    return token_error(scanner, "Only 'true' and 'false' are valid boolean keywords");
 }
 
-static Token null(Scanner *scanner)
+static Token token_null(Scanner *scanner)
 {
     const char *null_str = "null";
     char debugstr[120];
     if (strncmp(null_str, scanner->current - 1, strlen(null_str)) == 0)
     {
-        while (peek(scanner) != 'l') advance(scanner);
-        advance(scanner);
-        advance(scanner);
-        return make_token(TOKEN_NULL_VAL, scanner);
+        while (peek(scanner) != 'l') scanner_advance(scanner);
+        scanner_advance(scanner);
+        scanner_advance(scanner);
+        return token_make(TOKEN_NULL_VAL, scanner);
     }
 
     strncpy(debugstr, scanner->start, scanner->current - scanner->start);
     printf("DEBUG: %s\n", debugstr);
 
-    return error_token(scanner, "Unexpected keyword");
+    return token_error(scanner, "Only 'null' is a valid keyword for the null value");
 }
 
 static Token scan_token(Scanner *scanner)
 {
-    skip_white_space(scanner);
+    scanner_skip_white_space(scanner);
     scanner->start = scanner->current;
 
-    if (is_at_end(scanner)) return make_token(TOKEN_EOF, scanner);
+    if (scanner_is_at_end(scanner)) return token_make(TOKEN_EOF, scanner);
 
-    char c = advance(scanner);
-    if (is_digit(c)) return number(scanner);
+    char c = scanner_advance(scanner);
+    if (is_digit(c)) return token_number(scanner);
 
     switch(c)
     {
-        case '{': return make_token(TOKEN_LEFT_BRACE, scanner);
-        case '}': return make_token(TOKEN_RIGHT_BRACE, scanner);
-        case '[': return make_token(TOKEN_LEFT_BRACKET, scanner);
-        case ']': return make_token(TOKEN_RIGHT_BRACKET, scanner);
-        case ',': return make_token(TOKEN_COMMA, scanner);
-        case ':': return make_token(TOKEN_COLON, scanner);
-        case '"': return string(scanner, '"');
-        case '\'': return string(scanner, '\'');
-        case 't': return boolean(scanner);
-        case 'f': return boolean(scanner);
-        case 'n': return null(scanner);
+        case '{': return token_make(TOKEN_LEFT_BRACE, scanner);
+        case '}': return token_make(TOKEN_RIGHT_BRACE, scanner);
+        case '[': return token_make(TOKEN_LEFT_BRACKET, scanner);
+        case ']': return token_make(TOKEN_RIGHT_BRACKET, scanner);
+        case ',': return token_make(TOKEN_COMMA, scanner);
+        case ':': return token_make(TOKEN_COLON, scanner);
+        case '"': return token_string(scanner);
+        case 't': return token_boolean(scanner);
+        case 'f': return token_boolean(scanner);
+        case 'n': return token_null(scanner);
     }
-    return error_token(scanner, "Unexpected Character.");
+    return token_error(scanner, "Unexpected Character.");
 }
+
+static Parser* parser_init()
+{
+    Parser *parser = (Parser*)malloc(sizeof(Parser));
+    parser->depth = 0;
+    parser->had_error = false;
+    return parser;
+}
+
+static void parser_advance(Parser *parser, Scanner *scanner)
+{
+    parser->previous = parser->current;
+
+    for (;;)
+    {
+        parser->current = scan_token(scanner);
+        if (parser->current.type != TOKEN_ERROR) break;
+        char *debug_str = malloc((sizeof(char) * parser->current.length) + 1);
+        strncpy(debug_str, parser->current.start, parser->current.length);
+        printf("Error at line %i: %s\n", parser->current.line, debug_str);
+    }
+}
+
 
 int main()
 {
@@ -225,7 +259,7 @@ int main()
             \"toys\": false         \n\
         },                          \n\
         {                           \n\
-            \"name\": \"August\",   \n\
+            \"name\": \"Augu\\nst\",   \n\
             \"age\": 7              \n\
             \"toys\": null          \n\
         }                           \n\
@@ -234,19 +268,33 @@ int main()
 
     printf("DEBUG STRING:\n%s\n", source);
 
-    Scanner *scanner = init_scanner(source);
-    Token token;
-    while (token.type != TOKEN_EOF)
+    Scanner *scanner = scanner_init(source);
+    Parser *parser = parser_init();
+//    Token token;
+//    while (token.type != TOKEN_EOF)
+//    {
+//        token = scan_token(scanner);
+//        TokenType type = token.type;
+//        int line = token.line;
+//        const char *start = token.start;
+//        int length = token.length;
+//
+//        char *debug_str = malloc(sizeof(char) * 256);
+//        strncpy(debug_str, start, length);
+//
+//        printf("DEBUG: type: %d, line: %d, value: %s, length: %d\n", type, line, debug_str, length);
+//    }
+
+    while (parser->current.type != TOKEN_EOF)
     {
-        token = scan_token(scanner);
-        TokenType type = token.type;
-        int line = token.line;
-        const char *start = token.start;
-        int length = token.length;
+        parser_advance(parser, scanner);
+        TokenType type = parser->current.type;
+        int line = parser->current.line;
+        const char *start = parser->current.start;
+        int length = parser->current.length;
 
-        char *debug_str = malloc(sizeof(char) * 256);
+        char *debug_str = malloc((sizeof(char) * length) + 1);
         strncpy(debug_str, start, length);
-
-        printf("DEBUG: type: %d, line: %d, value: %s, length: %d\n", type, line, debug_str, length);
+        printf("DEBUG PARSER: type: %d, line: %d, value: %s, length: %d\n", type, line, debug_str, length);
     }
 }
